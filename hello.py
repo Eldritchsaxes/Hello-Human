eecb3e5dd6e1eb0327ee5ebe3bbc450cee4bf2d7
Eldritchsaxes <114213932+Eldritchsaxes@users.noreply.github.com>
2022-09-28 17:20:01 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+# This program says hello and asks for my name.
+print(' Hello human! ')
+print(' Please tell me. What is your name? ')
+Myname = input()
+print(' It is good to meet you, ' + Myname)
